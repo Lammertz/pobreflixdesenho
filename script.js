47d3080806052ccdd803fe64355d13fa5552bf18
Lammertz <39447969+Lammertz@users.noreply.github.com>
2023-11-28 17:31:34 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,30 @@
+// script.js
+var player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '409',
+    width: '727',
+    videoId: 'Pj9YnrOxo-w', // Substitua com o ID real do seu vídeo do YouTube
+    playerVars: {
+      'autoplay': 1,
+      'controls': 0,
+      'showinfo': 0,
+      'rel': 0,
+    },
+    events: {
+      'onReady': onPlayerReady,
+    },
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.setPlaybackQuality('hd720');
+}
+// Adicione ao seu arquivo de script (script.js)
+var overlay = document.getElementById('overlay');
+
+function startVideo() {
+  var videoWrapper = document.querySelector('.video-wrapper');
+  videoWrapper.removeChild(overlay);
+}
